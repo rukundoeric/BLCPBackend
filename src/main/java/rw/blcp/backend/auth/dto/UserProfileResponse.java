7f6244fede2b55b06bde79c6032f6b8ea74910d4
Rukundo Eric <ericrukundo005@gmail.com>
2026-05-09 19:35:04 +0200

    Add fetch user profile

---
+++ src/main/java/rw/blcp/backend/auth/dto/UserProfileResponse.java
@@ -0,0 +1,7 @@
+package rw.blcp.backend.auth.dto;
+
+import java.util.Set;
+import rw.blcp.backend.auth.RoleName;
+
+public record UserProfileResponse(
+        String email, String firstName, String lastName, Set<RoleName> roles) {}
